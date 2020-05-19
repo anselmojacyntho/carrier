71d407a03797dc34322343e97b068f5c92b601c3
Anselmo Barbosa <anselmojacyntho@gmail.com>
2020-05-19 16:51:07 -0300

    Classe com os metodos de busca na api do IBGE

---
+++ src/IBGEService.php
@@ -0,0 +1,55 @@
+<?php namespace AnselmoJacyntho\Carrier;
+
+use AnselmoJacyntho\Carrier\Curl;
+
+class IBGEService {
+
+    protected $endpoint;
+
+    public function __construct()
+    {
+        $this->endpoint = 'https://servicodados.ibge.gov.br/api/v1/localidades/';
+    }
+
+    public function getRegions()
+    {
+        $response = Curl::get("$this->endpoint/regioes");
+
+        return json_decode($response);
+    }
+
+    public function getStatesByRegion($region_id)
+    {
+        $response = Curl::get("$this->endpoint/regioes/$region_id/estados");
+
+        return json_decode($response);
+    }
+
+    public function getStates()
+    {
+        $response = Curl::get("$this->endpoint/estados");
+
+        return json_decode($response);
+    }
+
+    public function getCities()
+    {
+        $response = Curl::get("$this->endpoint/municipios");
+
+        return json_decode($response);
+    }
+
+    public function getCitiesByState($state_id)
+    {
+        $response = Curl::get("$this->endpoint/estados/$state_id/municipios");
+
+        return json_decode($response);
+    }
+
+    public function getDistrictsByCity($city_id)
+    {
+        $response = Curl::get("$this->endpoint/municipios/$city_id/distritos");
+
+        return json_decode($response);
+    }
+}
