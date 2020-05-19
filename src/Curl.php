1fe03d934c0b7d755c8f57d4e0eea0c3eda95152
Anselmo Barbosa <anselmojacyntho@gmail.com>
2020-05-19 16:34:33 -0300

    Criacao da classe de curl para os requests get

---
+++ src/Curl.php
@@ -0,0 +1,15 @@
+<?php namespace AnselmoJacyntho\Carrier;
+
+class Curl {
+
+    public static function get ( $endpoint )
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $endpoint);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+}
